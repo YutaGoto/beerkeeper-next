28254fed91ab772d2fe483502c74c4f0967fb5f0
YutaGoto <you.goto.510@gmail.com>
2022-01-20 00:21:36 +0900

    refactor: add axios default config

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,9 +1,6 @@
-import axios from "axios";
+import { axios } from "./axios";
 
 async function fetcher(url: string, token: string) {
-  axios.defaults.baseURL = process.env.BASE_URL;
-  axios.defaults.headers.common["content-type"] =
-    "application/json;charset=UTF-8";
   const res = await axios.get(url, {
     headers: { Authorization: `Bearer ${token}` },
   });
